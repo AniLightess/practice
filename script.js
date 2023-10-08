7b83d9d3151b2e1b50936502f0ea112d37491687
Oleksii Blizhnik <blizhnik.oleksey@gmail.com>
2023-10-08 12:42:58 +0300

    add remove active style

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let skill_point = 1;
 
 const onSkill_tree_item_click_style = (event) => {
 	let target = event.currentTarget;
-	if (skill_point > 0) {
+	if (target.classList.contains("active_shadow_border")) {
+		target.classList.remove("active_shadow_border");
+		skill_point++;
+	} else if (skill_point > 0) {
 		target.classList.add("active_shadow_border");
 		skill_point--;
 	}
